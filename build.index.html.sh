418c3a29c74a566e4ac58b8d5ef8a1e668ee8bba
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-04-06 22:08:43 -0400

    Remove unused script.

--- build.index.html.sh
+++
@@ -1,6 +0,0 @@
-      touch index.html ;               
-      for f in `ls full_size/` ; 	
-	do 
-	echo "<a href=\"preview/$f\"><img src=\"thumbs/thumb.$f\" /></a>" >> index.html;       
-      done ;       
-      
