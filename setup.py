033e20620c70db236befecebe9764f5b129442c4
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-26 23:41:05 -0400

    Fix typos in setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from setuptools import setup, find_packages
 
-setup(name='phototools,
+setup(name='phototools',
       version='1.0',
       description='Tools for manipulating the Boyd photo collection',
       author='Joshua D. Boyd',
       author_email='jdboyd@jdboyd.net',
       packages = find_packages(),
       entry_points = {'console_scripts': ['pt_sort = phototools.sort:run',
-                                          'pt_index_html = phototools.index_html:run,
+                                          'pt_index_html = phototools.index_html:run',
                                           ]}
      )
