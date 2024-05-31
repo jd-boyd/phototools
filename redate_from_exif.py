4a24c1ed0f19352b29866e531c581aead57a64c7
Joshua D. Boyd <jdboyd@jdboyd.net>
2024-05-30 23:15:20 -0400

    Update redate tool for heic files.

--- redate_from_exif.py
+++ redate_from_exif.py
@@ -1,10 +1,17 @@
 import sys
-from PIL import Image
-from PIL.ExifTags import TAGS
 from datetime import *
 from subprocess import check_output
 import time
 import os
+
+import dateutil
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+from pillow_heif import register_heif_opener
+import piexif
+
+register_heif_opener()
 
 def get_tag(req, img):
     xf = img._getexif()
@@ -34,6 +41,15 @@ for arg in sys.argv[1:] :
         date = dc_date[b"Timestamp"].strip().decode('utf-8')
         #Wed Feb 19 19:57:29 2014
         date = datetime(*(time.strptime(date, "%a %b %d %H:%M:%S %Y")[0:6]))
+    elif arg.lower().endswith("heic"):
+        i = Image.open(arg)
+        e = piexif.load(i.info['exif'], key_is_name=True)
+        try:
+            date = e["Exif"]["DateTimeOriginal"].decode()
+            date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
+        except Exception as e:
+            print("Failed to get DateTimeOriginal on", arg, e)
+            continue
     else:
         # Open the file
         img = Image.open(arg)
